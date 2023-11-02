d5ed8804a8317d794b817ba1ea54868496bb5a08
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-02 15:27:44 +0900

    fix

--- db/deleteTable.ts
+++ db/deleteTable.ts
@@ -1,7 +1,20 @@
 import { db } from "../utility/database.ts";
 
-const deleteResult = await db.execute(
-  `DROP TABLE IF EXISTS users;`,
-);
+try {
+  const transaction = await db.transaction();
 
-console.log(deleteResult);
+  // const tables = ["users", "todos"];
+
+  // for (const table of tables) {
+  //   await transaction.execute({
+  //     sql: `DROP TABLE IF EXISTS ?;`,
+  //     args: [table],
+  //   });
+  // }
+
+  await transaction.execute(`DROP TABLE IF EXISTS users;`);
+
+  await transaction.commit();
+} catch (error) {
+  console.error(error);
+}
