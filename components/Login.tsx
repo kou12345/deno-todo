e5fb6f2c69e45045d04c9c858e75fa2dddcce33b
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-03 21:55:25 +0900

    fix ui

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,18 +2,34 @@ export function Login() {
   return (
     <div className="w-3/4 mx-auto">
       <form
-        className="flex flex-col"
+        className="flex flex-col items-center"
         method="post"
         action="/api/login"
       >
-        user name:{" "}
-        <input className="border" type="text" name="username" required />
-        password:{" "}
-        <input className="border" type="password" name="password" required />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          type="submit"
-        >
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">UserName</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            required
+          />
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="Password"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            required
+          />
+        </div>
+
+        <button className="my-6" type="submit" class="btn btn-primary">
           Submit
         </button>
       </form>
