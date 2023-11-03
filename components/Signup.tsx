ebf2fce489d83d8f52d450e58018276773ad01dc
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-03 13:48:44 +0900

    add component Signup

---
+++ components/Signup.tsx
@@ -0,0 +1,20 @@
+export function Signup() {
+  return (
+    <div className="w-3/4 mx-auto">
+      <form
+        className="flex flex-col"
+        method="post"
+        action="/api/signup"
+      >
+        user name: <input className="border" type="text" name="username" />
+        password: <input className="border" type="password" name="password" />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
